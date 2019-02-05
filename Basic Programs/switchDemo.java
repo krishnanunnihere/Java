03c4ec50860b4ba6faefd39e0d27b45fa1575e3f
krishnanunnihere <krishnanunnitk1999@gmail.com>
2019-02-05 23:47:19 +0530

    Switch statement

---
+++ Basic Programs/switchDemo.java
@@ -0,0 +1,35 @@
+class switchDemo
+{
+    public static void main(String args[])
+    {
+        int i;
+        for(i=0;i<10;i++)
+        switch(i)
+        {
+            case 0:
+            System.out.println("i is 0");
+            break;
+
+            case 1:
+            System.out.println("i is 1");
+            break;
+
+            case 2:
+            System.out.println("i is 2");
+            break;
+
+            case 3:
+            System.out.println("i is 3");
+            break;
+
+            case 4:
+            System.out.println("i is 4");
+            break;
+
+            default:
+            System.out.println("i is 5 or more");
+
+
+        }
+    }
+}
