8818ef8d1aaeb2a465b6a2cb41033b50fa91848b
krishnanunnihere <krishnanunnitk1999@gmail.com>
2019-02-06 23:40:58 +0530

    no body loop

---
+++ Basic Programs/noBodyLoop.java
@@ -0,0 +1,11 @@
+class noBodyLoop
+{
+    public static void main(String args[])
+    {
+        int i;
+        int sum=0;
+        //sum the numbers through 5
+        for(i=0;i<=5;sum+=i++);
+        System.out.println("Sum is "+sum);
+    }
+} 
