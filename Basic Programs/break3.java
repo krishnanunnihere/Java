840b14bd0fff18f9af5bf261d5bd6acbd765e49e
krishnanunnihere <krishnanunnitk1999@gmail.com>
2019-02-09 00:07:02 +0530

    break3

---
+++ Basic Programs/break3.java
@@ -0,0 +1,22 @@
+class break3
+{
+    public static void main(String args[])
+    {
+        for(int i=0;i<3;i++)
+        {
+            System.out.println("Outer loop count:"+i);
+            System.out.print("Inner loop count:");
+            int t=0;
+            while(t<100)
+            {
+                if(t==10)break;
+                System.out.print(t+" ");
+                t++;
+
+            }
+            
+
+        }
+        System.out.println("Loops complete.");
+    }
+}
